e0868665f0cbbcaed86e5e8204e830aa4cd9ae67
Kyle Stetz <kylestetz@gmail.com>
2013-01-24 07:52:30 -0500

    sending less messages; added active/inactive triangle

--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 
-	var socket = io.connect('clrclb.nodejitsu.com');
+	// var socket = io.connect('clrclb.nodejitsu.com');
+	var socket = io.connect('http://localhost');
+
 	socket.on('color event', function (data) {
-		// console.log("got a color event on the client side: " + data.id + "  " + data.color);
 		$('#'+data.id).css({ 'background-color': data.color });
+	});
+
+	socket.on('set active', function(data) {
+		$('#'+data.id).toggleClass('in-use', true);
+	});
+
+	socket.on('set inactive', function(data) {
+		$('#'+data.id).toggleClass('in-use', false);
 	});
 
 	var colorboxes = $('.colorbox');
@@ -15,17 +24,23 @@ $(document).ready(function() {
 		var this_id = $(this).attr('id');
 		$(this).spectrum({
 			clickoutFiresChange: true,
-			showAlpha: true,
+			showAlpha: false,
 			change: function(color){
 				// this_colorbox.css({'background-color':color.toHexString()});
 				socket.emit('changed color', { 'id':this_id, 'color':color.toHexString() });
 			},
 			move: function(color){
-				this_colorbox.css({'background-color':color.toHexString()});
-				if( $.now() - last_msg > 33 ){
+				// this_colorbox.css({'background-color':color.toHexString()});
+				if( $.now() - last_msg > 100 ){
 					socket.emit('changed color', { 'id':this_id, 'color':color.toHexString() });
 					last_msg = $.now();
 				}
+			},
+			show: function(color){
+				socket.emit('make active', { 'id': this_id });
+			},
+			hide: function(color){
+				socket.emit('make inactive', { 'id': this_id });
 			}
 		});
 	});
